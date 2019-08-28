bddc57b44a85cb87121e1c6f37f49b5815e6a6ff
Julio <kudamono200659@gmail.com>
2019-08-27 23:31:57 -0500

    Subiendo Archivos

---
+++ Javier/js/menu.js
@@ -0,0 +1,8 @@
+'use strict'
+var btnMenu=document.getElementById("btn-menu");
+var nav=document.getElementById("nav");
+
+btnMenu.addEventListener('click',function(){
+    nav.classList.toggle("mostrar");
+})
+   
