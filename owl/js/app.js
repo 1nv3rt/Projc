7b7b55eb604dbfd0142bb716d4142133d0a3e35b
Julio <kudamono200659@gmail.com>
2019-09-02 18:40:38 -0500

    MENU

--- owl/js/app.js
+++ owl/js/app.js
@@ -22,6 +22,16 @@ var swiper = new Swiper('.swiper-container', {
 	  autoplay: {
 		delay: 2000,
 	  },
-	  
-	  
   });
+  let ubicacionPrincipal = window.pageYOffset;
+  window.onscroll = function(){
+	  let Desplazamiento_Actual = window.pageYOffset;
+	  if(ubicacionPrincipal >= Desplazamiento_Actual){
+		document.getElementById('menu').style.top = '0';
+	  }else{
+		document.getElementById('menu').style.top = '-100px';
+	  }
+	  ubicacionPrincipal = Desplazamiento_Actual;
+  }
+
+  
